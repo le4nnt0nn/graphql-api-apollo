7314573782149a4efb5a898ce6be128bf9814bf6
Antonio <ditacj78@gmail.com>
2021-12-19 18:48:31 +0100

    init service

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+
+    type Query {
+        hello: String
+    }
+
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => {
+          return `Listening in ${process.env.PORT}`  
+        }
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen({port: process.env.PORT}).then(({url}) => {
+    console.log(`Servidor iniciado en ${url}`);
+});
+
 console.log(`Listening in ${process.env.PORT}`);
