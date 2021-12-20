8dc7940176158f4786afb68bf515eba67dcc00e1
Antonio <ditacj78@gmail.com>
2021-12-20 18:24:25 +0100

    formatted and added new Queries

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const { ApolloServer, gql } = require('apollo-server');
 const { join } = require('path');
 const { loadTypedefsSync } = require('@graphql-tools/load');
 const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
+const fs = require('fs');
+
+const aeropuertos = JSON.parse(fs.readFileSync(join(__dirname, './dataset.json'), 'utf-8'));
+
+console.log(aeropuertos);
 
 const sources = loadTypedefsSync(join(__dirname, './typeDefs.gql'), {
     loaders: [
@@ -13,8 +18,20 @@ const typeDefs = sources.map(source => source.document)
 
 const resolvers = {
     Query: {
-        listarAeropuertos: () => {
-            return [{id: "1", localizacion: "Sevilla", rutas: [], aviones: []}]
+        listarAeropuertos: () => {return aeropuertos},
+        obtenerAeropuertoPorId: (obj, args) => {
+            const response = aeropuertos.filter(aeropuerto => {
+                aeropuerto.id === args.id;
+            })
+            return response[0];
+        },
+        obtenerAeropuerto: (obj, { id, localizacion }) => {
+            const response = aeropuertos.filter(aeropuerto => {
+                if (aeropuerto.localizacion === localizacion || aeropuerto.id === id) {
+                    return aeropuerto;
+                }
+            });
+            return response[0];
         }
     }
 };
